51198b34ddb94e2e2bce3730c198e022a4053018
=charlesdadi <charles-abner.dadi@ekimetrics.com>
2014-12-08 11:34:16 +0100

    data

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ window.myTweetSearchEngine = angular.module('myTweetSearchEngine', ['elasticsear
  * Create a service to power calls to Elasticsearch. We only need to
  * use the _search endpoint.
  */
-myTweetSearchEngine.factory('recipeService',
+myTweetSearchEngine.factory('tweetsService',
     ['$q', 'esFactory', '$location', function($q, elasticsearch, $location){
         var client = elasticsearch({
             host: $location.host() + ":9200"
@@ -69,22 +69,26 @@ myTweetSearchEngine.factory('recipeService',
 /**
  * Create a controller to interact with the UI.
  */
-myTweetSearchEngine.controller('recipeCtrl',
-    ['recipeService', '$scope', '$location', function(tweets, $scope, $location){
+myTweetSearchEngine.controller('tweetsCtrl',
+    ['tweetsService', '$scope', '$location', function(tweets, $scope, $location){
         // Provide some nice initial choices
-        var initChoices = [
-            "data",
-            "luxe",
-            "vuitton",
-            "chanel",
-            "ekimetrics"
-         ];
+        var initChoices =  ['photography','luxury','stats','bigdata','ekimetrics',
+              'sport','fashion','multimedia','machinelearning','math'] ;
         var idx = Math.floor(Math.random() * initChoices.length);
 
         // Initialize the scope defaults.
         $scope.tweets = [];        // An array of recipe results to display
         $scope.page = 0;            // A counter to keep track of our current page
         $scope.allResults = false;  // Whether or not all results have been found.
+
+
+        $scope.sortChoice='-user.expert';
+        $scope.sortoptions=[
+            {label:'Followers count Down',key:'-user.followers_count'},
+            {label:'Followers count Up',key:'user.followers_count'},
+            {label:'Screen Name A-Z',key:'user.screen_name'},
+            {label:'Screen Name Z-A',key:'-user.screen_name'}
+        ];
 
         // And, a random search term to start if none was present on page load.
         $scope.searchTerm = $location.search().q || initChoices[idx];
